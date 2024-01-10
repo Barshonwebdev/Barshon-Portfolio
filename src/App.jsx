360bcb21f0d6a88a45f0d8ed3cf89f0547039a0c
Barshon <barshonweb@gmail.com>
2024-01-10 18:29:45 +0600

    about section complete (responsive + animation)

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,98 @@
 import About from "./components/About";
-import Header from "./components/Header";
 import Home from "./components/Home";
-
+import { FaGithub } from "react-icons/fa";
+import ThemeToggler from "./components/ThemeToggler";
 const App = () => {
   return (
     <div>
-      <Header></Header>
-      <Home></Home>
-      <About></About>
+      <div className="sticky top-0 z-10">
+        <div className="navbar bg-base-100">
+          <div className="navbar-start">
+            <ThemeToggler></ThemeToggler>
+            <div className="dropdown">
+              <div
+                tabIndex={0}
+                role="button"
+                className="btn btn-ghost lg:hidden"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h8m-8 6h16"
+                  />
+                </svg>
+              </div>
+              <ul
+                tabIndex={0}
+                className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+              >
+                <li>
+                  <a href="#home">Home</a>
+                </li>
+                <li>
+                  <a href="#about">About</a>
+                </li>
+                <li>
+                  <a>Resume</a>
+                </li>
+                <li>
+                  <a>Projects</a>
+                </li>
+                <li>
+                  <a>Skills</a>
+                </li>
+                <li>
+                  <a>Contact</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="navbar-center hidden lg:flex">
+            <ul className="menu menu-horizontal px-1">
+              <li>
+                <a href="#home" className="text-lg">Home</a>
+              </li>
+              <li>
+                <a href="#about" className="text-lg">
+                  About
+                </a>
+              </li>
+              <li>
+                <a className="text-lg">Resume</a>
+              </li>
+              <li>
+                <a className="text-lg">Projects</a>
+              </li>
+              <li>
+                <a className="text-lg">Skills</a>
+              </li>
+              <li>
+                <a className="text-lg">Contact</a>
+              </li>
+            </ul>
+          </div>
+          <div className="navbar-end">
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://github.com/Barshonwebdev"
+              className="btn"
+            >
+              <FaGithub className="text-4xl"></FaGithub>
+            </a>
+          </div>
+        </div>
+      </div>
+      <Home id="home"></Home>
+      <About id="about"></About>
     </div>
   );
 };
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,17 +1,63 @@
 import React from 'react';
 import aboutimg from '../assets/aboutimg-PhotoRoom-removebg-preview.png'
-const About = () => {
+import { FaPhone } from "react-icons/fa";
+import { IoMail } from "react-icons/io5";
+import { FaMapMarkerAlt } from "react-icons/fa";
+import { FaDownload } from "react-icons/fa";
+import { Flip, Slide } from 'react-awesome-reveal';
+import resumePDF from '../assets/Barshon Chowdhury- Frontend Web Developer Resume.pdf';
+const About = ({id}) => {
     return (
-        <div className='p-10 bg-stone-900 flex justify-center items-center text-white'>
-            <div className='flex flex-col lg:flex-row'>
-                <div className='w-64 h-64 rounded-full border-2 bg-gray-900 order-2 lg:order-1'>
-                    <img className=' mx-auto my-6 ' src={aboutimg} alt="" />
+      <div id={id} className="p-10 lg:p-20 bg-stone-900 flex  text-white">
+        <div className="flex justify-between items-center lg:space-x-56  flex-col lg:flex-row">
+          <Slide direction="left">
+            <div className="w-full">
+              <div className="">
+                <h3 className="uppercase text-xl">About me</h3>
+                <p className="mt-3 mb-7 text-gray-400">
+                  Currently a fresh CSE graduate, have worked on several
+                  personal webapp projects and eager to do more. Would like to
+                  participate on projects of reputed companies. Proficient in
+                  ReactJs, ExpressJs, NodeJs and Mongodb. Focused, fast learner
+                  and curious individual.{" "}
+                </p>
+              </div>
+              <div className="">
+                <div className="space-y-2">
+                  <h3 className="uppercase text-xl">Contact details</h3>
+                  <p className="text-gray-400">Barshon Chowdhury</p>
+                  <p className="text-gray-400">
+                    <FaMapMarkerAlt className="inline"></FaMapMarkerAlt> Japan
+                    Garden City,Ring Road, Mohammadpur
+                  </p>
+                  <p className="text-gray-400">Dhaka, Bangladesh</p>
+                  <p className="text-gray-400">
+                    <FaPhone className="inline"></FaPhone> +880-153-414-0507
+                  </p>
+                  <p className="text-gray-400">
+                    <IoMail className="inline"></IoMail> barshonweb@gmail.com
+                  </p>
                 </div>
-                <div className='order-1 lg:order-2'>
-                    s
+                <div className="flex justify-center lg:justify-normal mt-5">
+                  <a href={resumePDF} download="Barshon Chowdhury Resume (Frontend Developer)" target='_blank' rel='noreferrer'>
+                    <button className="btn ">
+                      <FaDownload className="text-xl"></FaDownload>Download
+                      Resume
+                    </button>
+                  </a>
                 </div>
+              </div>
             </div>
+          </Slide>
+          <Flip direction='vertical'>
+            <div className="lg:w-full my-10 ">
+              <div className="w-64 h-64 rounded-full border-2 bg-gray-900  ">
+                <img className=" mx-auto my-6 " src={aboutimg} alt="" />
+              </div>
+            </div>
+          </Flip>
         </div>
+      </div>
     );
 };
 

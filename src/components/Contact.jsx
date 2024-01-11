a24f7f4297eedd8564476afb01e160e45d137ca4
Barshon <barshonweb@gmail.com>
2024-01-11 18:39:51 +0600

    contact section design (complete + responsive)

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,23 +9,57 @@ const Contact = ({id}) => {
           <h2 className="text-3xl text-center mt-12 mb-2 font-semibold">
             Contact Me
           </h2>
-          <div className="flex justify-center">
-            <MdOutlineMail className="text-7xl"></MdOutlineMail>
-          </div>
-          <div className=" bg-slate-900 bg-opacity-30 lg:py-28 py-10 mx-5 px-5 lg:mx-96 my-5 rounded-lg flex flex-col md:flex-row justify-around items-center">
-            <form className="w-full">s</form>
+
+          <div className=" bg-slate-900 bg-opacity-30  py-10 mx-5 px-5 lg:mx-96 my-5 rounded-lg flex flex-col md:flex-row justify-around items-center space-y-7">
+            <form className="w-full flex flex-col items-center space-y-6">
+              <div className="">
+                <MdOutlineMail className="text-7xl"></MdOutlineMail>
+              </div>
+
+              <div className="flex space-x-3">
+                <label>
+                  Name <span className="text-red-600">*</span>
+                </label>
+                <input className="bg-white text-black rounded" type="text" />
+              </div>
+              <div className="flex space-x-3">
+                <label>
+                  Email <span className="text-red-600">*</span>
+                </label>
+                <input className="bg-white  text-black rounded" type="email" />
+              </div>
+              <div className="flex space-x-3">
+                <label>Subject</label>
+                <input className="bg-white  text-black rounded" type="text" />
+              </div>
+              <div className="flex space-x-1 ">
+                <label>
+                  Message<span className="text-red-600">*</span>
+                </label>
+                <textarea
+                  className="bg-white  text-black rounded"
+                  name=""
+                  id=""
+                  cols="22"
+                  rows="5"
+                ></textarea>
+              </div>
+
+              <div>
+                <button className='zoom btn btn-sm rounded-lg btn-neutral'>Send</button>
+              </div>
+            </form>
             <div className="space-y-2 w-full text-sm text-center ">
               <h3 className=" text-lg font-bold">Address & Phone</h3>
-              <p className="text-gray-400">Barshon Chowdhury</p>
-              <p className="text-gray-400">
+              <p className="">Barshon Chowdhury</p>
+              <p className="">
                 <FaMapMarkerAlt className="inline"></FaMapMarkerAlt> Japan
                 Garden City,Ring Road, Mohammadpur
               </p>
-              <p className="text-gray-400">Dhaka, Bangladesh</p>
-              <p className="text-gray-400">
+              <p className="">Dhaka, Bangladesh</p>
+              <p className="">
                 <FaPhone className="inline"></FaPhone> +880-153-414-0507
               </p>
-              
             </div>
           </div>
         </div>
